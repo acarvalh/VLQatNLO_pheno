3d344686d44707a00fbe871a336eac202014d15d
alexandra.oliveira@gmal.com <alexandra.oliveira@cern.ch>
2018-05-08 10:45:07 +0200

    linking the whole external

--- do_links.sh
+++ do_links.sh
@@ -3,8 +3,8 @@
 
 ln -s ../Delphes/classes/SortableObject.h SortableObject.h
 ln -s ../Delphes/classes/DelphesClasses.h DelphesClasses.h
-ln -s ../Delphes/classes/                 classes
-ln -s ../Delphes/external/ExRootAnalysis/ ExRootAnalysis
-ln -s ../Delphes/libDelphes.so            libDelphes.so
-ln -s ../Delphes/external/fastjet/        fastjet
-#ln -s ../Delphes/libFWCoreFWLite.so
+#ln -s ../Delphes/classes/                 classes
+#ln -s ../Delphes/external/ExRootAnalysis/ ExRootAnalysis
+#ln -s ../Delphes/libDelphes.so            libDelphes.so
+#ln -s ../Delphes/external/fastjet/        fastjet
+ln -s ../Delphes/external                 external
