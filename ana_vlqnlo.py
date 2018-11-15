1c7424fbd39faa7d506577ebb92a00bf06e50970
Devdatta Majumder <devdatta.majumder@cern.ch>
2018-11-15 15:00:16 +0100

    adding instructions to run

--- ana_vlqnlo.py
+++ ana_vlqnlo.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
+help = '''
+
+Print help:
+  python ana_vlqnlo.py
+
+For interactive jobs:
+
+  python ana_vlqnlo.py -f /path/to/delphes/tree.root -w /path/to/pdf_scale_weights.txt -n NumberOfEvents
+
+For batch submission:
+
+  python ana_vlqnlo.py -f /path/to/filenames.txt 
+'''
+
 DEBUG = False
 
 bashscripttemplate='''#!/bin/bash
@@ -9,7 +23,7 @@ cd RUNDIR
 source /afs/cern.ch/work/d/dm2/Analysis/VLQNLOPheno/VLQatNLO_pheno/setup.bash
 cd RUNDIR
 
-python /afs/cern.ch/work/d/dm2/Analysis/VLQNLOPheno/VLQatNLO_pheno/ana_vlqnlo.py -f FNAME
+python /afs/cern.ch/work/d/dm2/Analysis/VLQNLOPheno/VLQatNLO_pheno/ana_vlqnlo.py -f FNAME -w WTNAME
 
 #find -type l -delete
 
@@ -55,56 +69,7 @@ def isBTagged(jet):
 
 
 
-def analyze_gen(fname, maxEvents=-1):
-
-  import os
-
-  if fname[0] == "#": return
-
-  import ROOT
-  import numpy as np
-  ROOT.gROOT.SetBatch()
-  print('Analyzing {}'.format(fname))
-
-  ROOT.gInterpreter.Declare('#include "external/ExRootAnalysis/ExRootTreeReader.h"')
-  ROOT.gInterpreter.Declare('#include "DelphesClasses.h"')
-
-  ROOT.gSystem.Load("libDelphes")
-
-  chain = ROOT.TChain("Delphes")
-  try: chain.Add(fname)
-
-  except IOError as e:
-    print('Couldnt open the file (%s).' % e)
-    return -1
-
-  treeReader = ROOT.ExRootTreeReader(chain)
-  numberOfEntries = treeReader.GetEntries()
-
-  if DEBUG: print("The tree have {0} from file {1}".format(numberOfEntries, fname))
-
-  branchEvent    = treeReader.UseBranch("Event")
-  branchPart     = treeReader.UseBranch("Particle")
-
-  if maxEvents == -1:
-    toprocess = numberOfEntries
-  else:
-    toprocess = maxEvents
-
-  wtfile = open('/eos/cms/store/user/acarvalh/VLQNLO_files/withPDFunc/Results_20181106/Results_T_L_Qjq_4FNS_NLO_muonsWZHdecay/Events/ASCII/T_W_W_4FNS_Qjq_L_muonsWZHdecay_M1200GeV_Kprod0.1_Kdecay0.1_LHCEnergy13TeV_systematics.dat', 'read')
-  wtlines = wtfile.readlines()
-
-  for event in range(0, toprocess): #
-    treeReader.ReadEntry(event)
-    for part in branchPart:
-      print('part id = {}'.format(part.PID))
-      wts = wtlines[event+1]
-      #print(len(wts))
-
-
-
-
-def analyze(fname, maxEvents=-1):
+def analyze(fname, wtfilename="", maxEvents=-1):
 
   import os
 
@@ -139,17 +104,31 @@ def analyze(fname, maxEvents=-1):
   fout = ROOT.TFile("out_"+os.path.basename(fname), "recreate")
   fout.cd()
 
-  h_cutflow = ROOT.TH1D("h_cutflow", "", 5, -0.5, 4.5)
+  h_cutflow = ROOT.TH1D("h_cutflow", "", 7, -0.5, 6.5)
   h_cutflow.GetXaxis().SetBinLabel(1, "All")
-  h_cutflow.GetXaxis().SetBinLabel(2, " N(W jet)>0")
+  #h_cutflow.GetXaxis().SetBinLabel(2, " N(W jet)>0")
+  h_cutflow.GetXaxis().SetBinLabel(2, "N(mu)>0")
   h_cutflow.GetXaxis().SetBinLabel(3, "N(b jet)>0")
   h_cutflow.GetXaxis().SetBinLabel(4, "p_{T}(1st b jet)>200")
   h_cutflow.GetXaxis().SetBinLabel(5, "Fwd jet")
+  h_cutflow.GetXaxis().SetBinLabel(6, "MET")
+  h_cutflow.GetXaxis().SetBinLabel(7, "S_{T}")
 
-  h_HT              = ROOT.TH1D("h_HT"            , ";H_{T};Events/100 GeV;",20, 1000, 3000)
-  h_T_mass          = ROOT.TH1D("h_T_mass"        , ";M(T);Events/100 GeV;",20, 1000, 3000)
+  h_ST              = ROOT.TH1D("h_ST"            , ";S_{T};Events/100 GeV;",20, 600, 2600)
+  h_ST_scaleHi      = ROOT.TH1D("h_ST_scaleHi"    , ";S_{T};Events/100 GeV;",20, 600, 2600)
+  h_ST_scaleLo      = ROOT.TH1D("h_ST_scaleLo"    , ";S_{T};Events/100 GeV;",20, 600, 2600)
+  h_HT              = ROOT.TH1D("h_HT"            , ";H_{T};Events/100 GeV;",20, 600, 2600)
+  h_HT_scaleHi      = ROOT.TH1D("h_HT_scaleHi"    , ";H_{T};Events/100 GeV;",20, 600, 2600)
+  h_HT_scaleLo      = ROOT.TH1D("h_HT_scaleLo"    , ";H_{T};Events/100 GeV;",20, 600, 2600)
+  h_T_mass          = ROOT.TH1D("h_T_mass"        , ";M(T);Events/100 GeV;",26, 0, 2600)
+  h_T_mass_scaleHi  = ROOT.TH1D("h_T_mass_scaleHi", ";M(T);Events/100 GeV;",26, 0, 2600)
+  h_T_mass_scaleLo  = ROOT.TH1D("h_T_mass_scaleLo", ";M(T);Events/100 GeV;",26, 0, 2600)
+
   h_wjets_pt        = ROOT.TH1D("h_wjets_pt"      , ";p_{T}(W jets);Events/50 GeV;",20, 200., 1200.)
   h_wjet1_pt        = ROOT.TH1D("h_wjet1_pt"      , ";p_{T}(leading-p_{T} W jet);Events/50 GeV;",20, 200., 1200.)
+  h_genmu1_pt       = ROOT.TH1D("h_genmu1_pt"     , ";p_{T}(leading-p_{T} muon);Events/50 GeV;",20, 40., 1040.)
+  h_genmu1_eta      = ROOT.TH1D("h_genmu1_eta"    , ";#eta(leading-p_{T} muon);Events/50 GeV;",30, -3., 3.)
+  h_gennu1_pt       = ROOT.TH1D("h_gennu1_pt"     , ";p_{T}(leading-p_{T} neutrino);Events/50 GeV;",20, 40., 1040.)
   h_bjets_pt        = ROOT.TH1D("h_bjets_pt"      , ";p_{T}(b jets);Events/50 GeV;",24, 0., 1200.)
   h_bjet1_pt        = ROOT.TH1D("h_bjet1_pt"      , ";p_{T}(leading-p_{T} b jet);Events/50 GeV;",24, 0., 1200.)
   h_bjet1_fl        = ROOT.TH1D("h_bjet1_fl"      , ";Leading-p_{T} b jet flavour;Events/1 unit;",3,-0.5,5.5)
@@ -157,17 +136,19 @@ def analyze(fname, maxEvents=-1):
   h_nbjets          = ROOT.TH1D("h_nbjets"        , ";Number of b jets;Events/ 1 unit;", 6, -0.5, 5.5)
   h_forwardjet_pt   = ROOT.TH1D("h_forwardjet_pt" , ";p_{T}(forward jet);Events/10 GeV;",20, 0., 200.)
   h_forwardjet_eta  = ROOT.TH1D("h_forwardjet_eta", ";#eta(forward jet);Events/ 0.2 units;",50, -5, 5)
-  h_mu_pt   = ROOT.TH1D("h_mu_pt" , ";p_{T}(mu);Events/10 GeV;",20, 0., 200.)
-  h_mu_eta  = ROOT.TH1D("h_mu_eta", ";#eta(mu);Events/ 0.2 units;",50, -5, 5)
 
   if maxEvents == -1:
     toprocess = numberOfEntries
   else:
     toprocess = maxEvents
 
-  wtfile = open(fname.replace('root', 'dat').replace('Events', 'Events/ASCII')\
-      .replace('_pythia8_events', '_systematics'), \
-      'read')
+  if wtfilename == "":
+    wtfile = open(fname.replace('root', 'dat').replace('Events', 'Events/ASCII')\
+        .replace('_pythia8_events', '_systematics'), \
+        'read')
+  else:
+    wtfile = open(wtfilename,
+        'read')
   wtlines = wtfile.readlines()
 
   for event in range(0, toprocess):
@@ -178,25 +159,29 @@ def analyze(fname, maxEvents=-1):
     wts = wtlines[event+1]
     wts = wts.split()
     #print(len(wts))
-    wt_scaleDouble = wts[4]
-    wt_scaleHalf = wts[8]
+    wt = float(wts[9])
+    wt_scaleHi = float(wts[4])/wt
+    wt_scaleLo = float(wts[8])/wt
 
     h_cutflow.Fill(0)
 
     ### muons
-    muons = []
+    genmuons = []
+    gennus = []
     for part in range(0, branchParticle.GetEntries()):
-       genparticle =  branchParticle.At(part)
-       pdgCode = genparticle.PID
-       #print pdgCode
-       IsPU = genparticle.IsPU
-       status = genparticle.M1
-       #########################
-       if IsPU == 0 and (abs(pdgCode) == 13) and genparticle.Status == 23  :
-           #print (event, pdgCode, genparticle.Status)
-           if ( genparticle.PT > 25 and abs(genparticle.Eta) < 2.5 ) : muons.append(genparticle)
-       #if IsPU == 0 and (abs(pdgCode) == 14) and genparticle.Status == 23  :
-       #     print (event, pdgCode, genparticle.Status)
+       
+      genparticle =  branchParticle.At(part)
+      IsPU = genparticle.IsPU
+      status = genparticle.Status
+      pdgCode = genparticle.PID
+      if IsPU == 0 and status in [1, 23]:
+
+          mom1 = branchParticle.At(genparticle.M1)
+          if abs(mom1.PID) == 24:
+            if abs(pdgCode) == 13 and genparticle.PT > 40 and abs(genparticle.Eta) < 2.4: 
+              genmuons.append(genparticle)
+            if abs(pdgCode) in [12,14,16] and genparticle.PT > 50.:
+              gennus.append(genparticle)
 
     ### W jets
     wjets = []
@@ -212,16 +197,18 @@ def analyze(fname, maxEvents=-1):
         h_wjets_pt.Fill(jet.PT)
 
     bjets = []
+    HT = 0
     for jet in branchJet:
       if (jet.PT > 50. \
           and abs(jet.Eta) < 2.5 \
           ):
         p4_jet = jet.P4()
-        for wjet in wjets:
-          p4_wjet = wjet.P4()
-          if p4_jet.DeltaR(p4_wjet) < 1.2:
-            continue
-        for muon in muons :
+        HT += p4_jet.Pt()
+        #for wjet in wjets:
+        #  p4_wjet = wjet.P4()
+        #  if p4_jet.DeltaR(p4_wjet) < 1.2:
+        #    continue
+        for muon in genmuons :
             p4_muon = muon.P4()
             if p4_jet.DeltaR(p4_muon) < 0.4:
               continue
@@ -248,14 +235,12 @@ def analyze(fname, maxEvents=-1):
           h_bjets_pt.Fill(jet.PT)
 
     forwardjets = []
-    HT = 0
     for jet in branchJet:
       if (jet.PT > 20 \
           and abs(jet.Eta) > 1.5 \
           and abs(jet.Eta) < 5 \
           ):
         p4_jet = jet.P4()
-        HT += p4_jet.Pt()
         for wjet in wjets:
           p4_wjet = wjet.P4()
           if p4_jet.DeltaR(p4_wjet) < 1.2:
@@ -266,16 +251,25 @@ def analyze(fname, maxEvents=-1):
             continue
           forwardjets.append(jet)
 
+    print("HT = {}".format(HT))
+
+    genmuons.sort(key=Pt)
+    gennus.sort(key=Pt)
+    wjets.sort(key=Pt)
     wjets.sort(key=Pt)
     bjets.sort(key=Pt)
     forwardjets.sort(key=Eta)
 
     h_nwjets.Fill(len(wjets))
 
-    if len(wjets) < 1: continue
+    #if len(wjets) < 1: continue
+    if len(genmuons) < 1: continue
     h_cutflow.Fill(1)
 
-    h_wjet1_pt.Fill(wjets[0].PT)
+    if len(wjets) > 0: h_wjet1_pt.Fill(wjets[0].PT)
+    h_genmu1_pt.Fill(genmuons[0].PT)
+    h_genmu1_eta.Fill(genmuons[0].Eta)
+    if len(gennus) > 0: h_gennu1_pt.Fill(gennus[0].PT)
 
     h_nbjets.Fill(len(bjets))
 
@@ -299,14 +293,35 @@ def analyze(fname, maxEvents=-1):
     if abs(forwardjets[0].Eta) < 3.: continue
     h_cutflow.Fill(4)
 
-    ### Reconstruct T->Wb
-    h_T_mass.Fill( (wjets[0].P4() + bjets[0].P4()).Mag() )
-    h_HT.Fill(HT)
+    if len(gennus) < 1: continue
+    if gennus[0].PT < 50.: continue
+    #met = branchMET.MET
+    #if met < 50.: continue
+    h_cutflow.Fill(5) 
 
-    if len(muons) :
-        h_mu_pt.Fill(muons[0].PT)
-        h_mu_eta.Fill(muons[0].Eta)
-    print (event, len(muons), len(forwardjets), len(bjets))
+    ST = genmuons[0].PT + gennus[0].PT + bjets[0].PT
+
+    if ST < 500.: continue
+    h_cutflow.Fill(6)
+
+    ### Reconstruct T->Wb
+    #h_T_mass.Fill( (wjets[0].P4() + bjets[0].P4()).Mag() )
+
+    Minv = (genmuons[0].P4() + gennus[0].P4() + bjets[0].P4()).Mag()
+
+    #print("scaleHi = {0} scaleLo = {1}".format(wt_scaleHi, wt_scaleLo))
+
+    h_HT    .Fill(HT)
+    h_ST    .Fill(ST)
+    h_T_mass.Fill(Minv)
+
+    h_HT_scaleHi    .Fill(HT  , wt_scaleHi)
+    h_ST_scaleHi    .Fill(ST  , wt_scaleHi)
+    h_T_mass_scaleHi.Fill(Minv, wt_scaleHi)
+
+    h_HT_scaleLo    .Fill(HT  , wt_scaleLo)
+    h_ST_scaleLo    .Fill(ST  , wt_scaleLo)
+    h_T_mass_scaleLo.Fill(Minv, wt_scaleLo)
 
   fout.Write()
   fout.Close()
@@ -314,7 +329,7 @@ def analyze(fname, maxEvents=-1):
 
 
 
-def submit(sample, files,  maxEvents, queue):
+def submit(sample, files, wtfiles, maxEvents, queue):
   import os, re
 
   print(files)
@@ -331,9 +346,11 @@ def submit(sample, files,  maxEvents, queue):
   rundir = os.path.join(workdir, 'output')
 
   for f in files:
+    w = wtfiles[files.index(f)]
     bashscript = open(os.path.join(workdir,'input', os.path.basename(f.replace('.root', '.sh'))), 'w')
     bashscriptcontent = re.sub('RUNDIR', rundir, bashscripttemplate)
     bashscriptcontent = re.sub('FNAME', f, bashscriptcontent)
+    bashscriptcontent = re.sub('WTNAME', w, bashscriptcontent)
     bashscript.write(bashscriptcontent)
     bashscript.close()
 
@@ -361,11 +378,6 @@ def main():
   from argparse import ArgumentParser
   parser = ArgumentParser(description="Do -h to see usage")
 
-  parser.add_argument("-g", "--analyze_gen",
-      dest="analyze_gen",
-      action="store_true",
-      help="Process only GEN info")
-
   parser.add_argument("-b", "--batch",
       dest="batch",
       action="store_true",
@@ -383,6 +395,13 @@ def main():
       default="filestoprocess.txt",
       type=str,
       help="Name of text file containing names of ROOT files to process")
+
+  parser.add_argument("-w", "--wtfile",
+      dest="wtfile",
+      action="store",
+      default="",
+      type=str,
+      help="Name of text file containing names of weight files to process")
 
   parser.add_argument("-n", "--maxEvents",
       dest="maxEvents",
@@ -403,12 +422,14 @@ def main():
       samples = json.load(infile)
       for sample in samples:
         print(sample)
-        submit(sample, samples[sample]["files"], args.maxEvents, args.queue)
+        if "wtfiles" in samples[sample]:
+          submit(sample, samples[sample]["files"], samples[sample]["wtfiles"], args.maxEvents, args.queue)
+        else:
+          wtfiles = [""] * len( samples[sample]["files"] )
+          submit(sample, samples[sample]["files"], samples[sample]["wtfiles"], args.maxEvents, args.queue)
+
   else:
-    if args.analyze_gen == True:
-      analyze_gen(args.infile, args.maxEvents)
-    else:
-      analyze(args.infile, args.maxEvents)
+    analyze(args.infile, args.wtfile, args.maxEvents)
 
 
 
